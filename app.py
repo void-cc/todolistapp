ae22a45027748235d89b7f11f67e564f66e47399
void-cc <chloecornelissen@hotmail.com>
2024-05-10 16:11:48 +0200

    v1 before todo interaction optimalization

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
+from datetime import datetime
 from flask import Flask, render_template, request, redirect, url_for, session
+from createengine import sessiondatabase as sdb
+from databasemodels import TodoList
+from sqlalchemy import select
+from databasemodels import User
+import database_interactions.todo_interacts as ti
 
 app = Flask(__name__)
 
 app.secret_key = b'dwhbd'
+
+
+
+
+
 
 @app.route('/', methods=['GET', 'POST'])
 def homepage():
@@ -12,6 +23,38 @@ def homepage():
         try:
             if request.form["toevoegen"]:
                 session['todo'] += [request.form['toevoegen']]
+                # print(request.form['toevoegen-date'])
+                # if datetime.now().date() > datetime.fromisoformat(request.form['toevoegen-date']):
+                #     overdue = False
+                # else:
+                #     overdue = True
+
+                new_todo = TodoList(
+                    todo_text=request.form['toevoegen'],
+                    todo_made_time=datetime.now(),
+                    todo_date=datetime.fromisoformat(request.form
+                                                     ['toevoegen-date']),
+                    todo_date_overdue=False,
+                    todo_done=False,
+                    user_id=1,
+                )
+                sdb.add(new_todo)
+                sdb.commit()
+                session['todo'] = []
+                stmt = select(TodoList).where(TodoList.user_id == 1)
+                result = sdb.execute(stmt)
+                for user_obj in result.scalars():
+                    print(user_obj)
+                    session['todo'].append(
+                        {'todo_text': user_obj.todo_text,
+                         'todo_made_time': user_obj.todo_made_time,
+                         'todo_date': user_obj.todo_date,
+                         'todo_date_overdue': user_obj.todo_date_overdue,
+                         'todo_done': user_obj.todo_done,
+                         'user_id': user_obj.user_id,
+                         'id': user_obj.id
+                         }
+                    )
             else:
                 if session['todo']:
                     print('session not done anything')
@@ -22,14 +65,14 @@ def homepage():
         except:
             pass
 
-        # Het verwijderen van een todo
+        # Het verwijderen van een to_do
         try:
             session['todoid'] = request.form['todo-delete']
             del(session['todo'][int(session['todoid'])-1])
         except KeyError:
             pass
 
-        # Het verwijderen van een todo done
+        # Het verwijderen van een to_do done
         try:
             session['todoid'] = request.form['todo-done-delete']
             del(session['todo-done'][int(session['todoid'])-1])
@@ -68,6 +111,25 @@ def homepage():
 
 
     return render_template('index.html', todolist=session['todo'], todolistdone=session['todo-done'])
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login_page():
+    melding = ''
+    username = ''
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        stmt = select(User).where(User.name.in_([username]))
+        for user in sdb.scalars(stmt):
+            print(user.name)
+            if username == username and user.check_password(password):
+                homepage()
+                return
+                #return render_template('index.html', todolist=[], todolistdone=[])
+            else:
+                melding = 'Username or password is incorrect'
+    return render_template('login.html', melding=melding, username=username)
 
 
 if __name__ == '__main__':
--- createengine.py
+++ createengine.py
@@ -3,19 +3,8 @@ import databasemodels as dbm
 from sqlalchemy.orm import Session
 
 
-engine = create_engine('sqlite://', echo=True)
+engine = create_engine('sqlite+pysqlite:///todo.db', echo=True)
 dbm.Base.metadata.create_all(engine)
 
-with Session(engine) as session:
-    chloe = dbm.User(
-        name="Chloe",
-        fullname="Chloe Cornelissen",
-        addresses=[dbm.Address(email_adress="chloecornelissen@hotmail.com")],
-    )
-    admin = dbm.User(
-        name="admin",
-        fullname="admin",
-        addresses=[dbm.Address(email_adress="admin@admin.admin")]
-    )
-    session.add_all([chloe, admin])
-    session.commit()
+
+sessiondatabase = Session(engine)
